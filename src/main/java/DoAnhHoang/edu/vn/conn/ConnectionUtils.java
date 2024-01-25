d83c40b005abf8e9efca04ffc80c3dba97c326e2
cupperdeptrai <6789masoi@gmail.com>
2024-01-25 23:35:42 +0700

    lab4

---
+++ src/main/java/DoAnhHoang/edu/vn/conn/ConnectionUtils.java
@@ -0,0 +1,41 @@
+package DoAnhHoang.vn.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+	public static Connection getMySQLConnection() 
+			throws SQLException,
+			ClassNotFoundException {
+				
+				String hostName = "localhost";
+				String dbName = "lab04";
+				String UserName = "sa";
+				String Password = "123";
+				String connectionURL = "jdbc:mysql://"
+				+ hostName + ":3306/" + dbName;
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection conn = DriverManager.getConnection(
+				connectionURL,UserName,Password);
+				
+				return conn;
+			}
+	
+	
+	public static void closeQuietly(Connection conn) {
+		try {
+			conn.close();
+		}catch(Exception e) {
+		}
+	};
+	
+	public static void rollbackQuietly(Connection conn) {
+		try {
+			conn.rollback();
+		}catch(Exception e) {
+			
+		}
+	}
+	
+}
